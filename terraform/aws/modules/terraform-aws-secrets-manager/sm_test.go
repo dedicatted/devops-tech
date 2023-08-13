11caa1b3dfdc278a7ff902c1ddc2594ff91656d8
illshestopalov <ill.shestopalov@gmail.com>
2023-08-13 17:31:29 +0300

    testing

---
+++ terraform/aws/modules/terraform-aws-secrets-manager/sm_test.go
@@ -0,0 +1,103 @@
+package test
+
+import (
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/kms"
+	"github.com/stretchr/testify/assert"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/gruntwork-io/terratest/modules/terraform"
+)
+
+func CreateKMSKey(t *testing.T) {
+	// Create AWS credentials with environment variables
+	creds := credentials.NewStaticCredentials(os.Getenv("AWS_ACCESS_KEY_ID"), os.Getenv("AWS_SECRET_ACCESS_KEY"), "")
+
+	// Initialize an AWS session with custom credentials
+	sess, err := session.NewSession(&aws.Config{
+		Region:      aws.String("us-east-1"),
+		Credentials: creds,
+	})
+	if err != nil {
+		t.Fatalf("Error creating session: %s", err)
+	}
+
+	// Create a KMS service client
+	svc := kms.New(sess)
+
+	// Create the KMS key
+	resp, err := svc.CreateKey(&kms.CreateKeyInput{})
+	if err != nil {
+		t.Fatalf("Error creating KMS key: %s", err)
+	}
+
+	// Ensure the response contains the KeyMetadata
+	assert.NotNil(t, resp.KeyMetadata)
+
+	// Export the ARN of the created KMS key to an environment variable
+	kmsKeyARN := *resp.KeyMetadata.Arn
+	os.Setenv("TF_VAR_kms_key_arn", kmsKeyARN)
+
+	fmt.Println("KMS Key ID:", *resp.KeyMetadata.KeyId)
+	fmt.Println("KMS Key ARN:", kmsKeyARN)
+
+}
+
+func TestWAF(t *testing.T) {
+	// Construct the terraform options with default retryable errors to handle the most common
+	// retryable errors in terraform testing.
+	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+		// Set the path to the Terraform code that will be tested.
+		TerraformDir: "../terraform-aws-secrets-manager",
+	})
+
+	// Run "terraform init" and "terraform apply". Fail the test if there are any errors.
+	terraform.InitAndApply(t, terraformOptions)
+
+	// Clean up resources with "terraform destroy" at the end of the test.
+	defer func() {
+		terraform.Destroy(t, terraformOptions)
+		// Schedule KMS key deletion after destroying the infrastructure
+		err := scheduleKMSKeyDeletion(t)
+		if err != nil {
+			t.Fatalf("Error scheduling KMS key deletion: %s", err)
+		}
+	}()
+
+}
+
+func scheduleKMSKeyDeletion(t *testing.T) error {
+	// Create AWS credentials with environment variables
+	creds := credentials.NewStaticCredentials(os.Getenv("AWS_ACCESS_KEY_ID"), os.Getenv("AWS_SECRET_ACCESS_KEY"), "")
+
+	// Initialize an AWS session with custom credentials
+	sess, err := session.NewSession(&aws.Config{
+		Region:      aws.String("us-east-1"),
+		Credentials: creds,
+	})
+	if err != nil {
+		return err
+	}
+
+	// Create a KMS service client
+	svc := kms.New(sess)
+
+	// Retrieve the KMS key ARN from the environment variable
+	kmsKeyARN := os.Getenv("TF_VAR_kms_key_arn")
+
+	// Schedule key deletion
+	_, err = svc.ScheduleKeyDeletion(&kms.ScheduleKeyDeletionInput{
+		KeyId:               aws.String(kmsKeyARN),
+		PendingWindowInDays: aws.Int64(7), // Specify the number of days before the key is deleted
+	})
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("Scheduled KMS key deletion:", kmsKeyARN)
+	return nil
+}

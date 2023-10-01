aba1b8444b9154af3110839894ef836545f22d80
illshestopalov <ill.shestopalov@gmail.com>
2023-10-01 16:21:00 +0300

    testing

--- terraform/aws/modules/terraform-aws-eks/eks/variables.tf
+++ terraform/aws/modules/terraform-aws-eks/eks/variables.tf
@@ -553,7 +553,7 @@ variable "manage_aws_auth_configmap" {
 variable "create_aws_auth_configmap" {
   description = "Determines whether to create the aws-auth configmap. NOTE - this is only intended for scenarios where the configmap does not exist (i.e. - when using only self-managed node groups). Most users should use `manage_aws_auth_configmap`"
   type        = bool
-  default     = false
+  default     = true
 }
 
 variable "aws_auth_node_iam_role_arns_non_windows" {
